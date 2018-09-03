9d38296c281588515b421414563c9de2a78a1287
Tom Oborne <tpoborne@gmail.com>
2018-09-04 00:50:37 +0100

    Player changes

--- src/cardgameforhumans/Player.java
+++ src/cardgameforhumans/Player.java
@@ -6,17 +6,19 @@ import cardgameforhumans.factories.DeckFactory;
 
 public class Player {
 	
-	Deck playersDeck;
+	Deck deck;
+	Integer mana;
 	Integer hitpoints;
 	Integer armor;
 	Hand hand;
 	Graveyard graveyard;
-	private int openingHandSize = 5;
+	Integer openingHandSize = 5;
 	
 	public Player() {
-		playersDeck = DeckFactory.getRandomDeck(20);
+		deck = DeckFactory.getRandomDeck(20);
 		hitpoints = 30;
 		armor = 0;
+		mana = 1;
 		hand = drawOpeningHand();
 		graveyard = new Graveyard();
 	}
@@ -24,8 +26,58 @@ public class Player {
 	private Hand drawOpeningHand() {
 		Hand hand = new Hand();
 		for (int i = 0; i < openingHandSize; i++) {
-			hand.addCard(playersDeck.drawFromTop());
+			hand.addCard(deck.drawFromTop());
 		}
 		return hand;
 	}
+
+	public Deck getPlayersDeck() {
+		return deck;
+	}
+
+	public void setPlayersDeck(Deck playersDeck) {
+		this.deck = playersDeck;
+	}
+
+	public Integer getMana() {
+		return mana;
+	}
+
+	public void setMana(Integer mana) {
+		this.mana = mana;
+	}
+
+	public Integer getHitpoints() {
+		return hitpoints;
+	}
+
+	public void setHitpoints(Integer hitpoints) {
+		this.hitpoints = hitpoints;
+	}
+
+	public Integer getArmor() {
+		return armor;
+	}
+
+	public void setArmor(Integer armor) {
+		this.armor = armor;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public Graveyard getGraveyard() {
+		return graveyard;
+	}
+
+	public void setGraveyard(Graveyard graveyard) {
+		this.graveyard = graveyard;
+	}
+	
+	
 }
